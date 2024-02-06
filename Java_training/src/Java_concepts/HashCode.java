55cecf2e48b03482f6c81ebb936da4df850414c0
yyogesh0301 <101698207+yyogesh0301@users.noreply.github.com>
2024-02-06 11:56:43 +0530

    Add files via upload

---
+++ Java_training/src/Java_concepts/HashCode.java
@@ -0,0 +1,67 @@
+<<<<<<< HEAD
+package Java_concepts;
+import java.lang.reflect.Method;
+
+public class HashCode {
+
+	public void getSampleMethod() {}
+	 
+    // create main method
+    public static void main(String args[])
+    {
+ 
+        try {
+ 
+            // create class object for class name HashCode
+            Class c = HashCode.class;
+ 
+            // get Method object of method name getSampleMethod
+            Method method = c.getDeclaredMethod("getSampleMethod", null);
+ 
+            // get hashcode of method object using hashCode() method
+            int hashCode = method.hashCode();
+ 
+            // Print hashCode with method name
+            System.out.println("hashCode of method " + method.getName()
+                               + " is " + hashCode);
+        }
+        catch (Exception e) {
+            // print if any exception occurs
+            e.printStackTrace();
+        }
+    }
+}
+=======
+package Java_concepts;
+import java.lang.reflect.Method;
+
+public class HashCode {
+
+	public void getSampleMethod() {}
+	 
+    // create main method
+    public static void main(String args[])
+    {
+ 
+        try {
+ 
+            // create class object for class name HashCode
+            Class c = HashCode.class;
+ 
+            // get Method object of method name getSampleMethod
+            Method method = c.getDeclaredMethod("getSampleMethod", null);
+ 
+            // get hashcode of method object using hashCode() method
+            int hashCode = method.hashCode();
+ 
+            // Print hashCode with method name
+            System.out.println("hashCode of method " + method.getName()
+                               + " is " + hashCode);
+        }
+        catch (Exception e) {
+            // print if any exception occurs
+            e.printStackTrace();
+        }
+    }
+}
+>>>>>>> 993e728c6925ff6f35c145acc643240027a3c0d8
---
+++ Java_training/src/Java_concepts/Threads.java
@@ -0,0 +1,106 @@
+<<<<<<< HEAD
+package Java_concepts;
+
+/*
+ Here we are showing the concept of Multithreading.Here we are defining one class and 
+ a thread using Runnable interface Both threads print messages .
+ The main thread initiates these threads and finishes its execution afterward.
+ */
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("MyThread running");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("MyThread: " + i);
+            try {
+                Thread.sleep(1000); 
+            } catch (InterruptedException e) {
+                System.out.println("MyThread interrupted");
+            }
+        }
+        System.out.println("MyThread finished");
+    }
+}
+
+public class Threads {
+    public static void main(String[] args) {
+        System.out.println("Main thread started");
+
+        MyThread myThread = new MyThread();
+        myThread.start(); 
+
+        // Another thread using Runnable interface
+        Thread runnableThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("RunnableThread running");
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("RunnableThread: " + i);
+                    try {
+                        Thread.sleep(1000); // Sleep for 1 second
+                    } catch (InterruptedException e) {
+                        System.out.println("RunnableThread interrupted");
+                    }
+                }
+                System.out.println("RunnableThread finished");
+            }
+        });
+        runnableThread.start(); 
+
+        System.out.println("Main thread finished");
+    }
+}
+
+=======
+package Java_concepts;
+
+/*
+ Here we are showing the concept of Multithreading.Here we are defining one class and 
+ a thread using Runnable interface Both threads print messages .
+ The main thread initiates these threads and finishes its execution afterward.
+ */
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("MyThread running");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("MyThread: " + i);
+            try {
+                Thread.sleep(1000); 
+            } catch (InterruptedException e) {
+                System.out.println("MyThread interrupted");
+            }
+        }
+        System.out.println("MyThread finished");
+ 
+    }
+}
+
+public class Threads {
+    public static void main(String[] args) {
+        System.out.println("Main thread started");
+
+        MyThread myThread = new MyThread();
+        myThread.start(); 
+
+        // Another thread using Runnable interface
+        Thread runnableThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("RunnableThread running");
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("RunnableThread: " + i);
+                    try {
+                        Thread.sleep(1000); // Sleep for 1 second
+                    } catch (InterruptedException e) {
+                        System.out.println("RunnableThread interrupted");
+                    }
+                }
+                System.out.println("RunnableThread finished");
+            }
+        });
+        runnableThread.start(); 
+
+        System.out.println("Main thread finished");
+    }
+}
+
+>>>>>>> 993e728c6925ff6f35c145acc643240027a3c0d8
